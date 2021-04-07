db81f5d7337c0f21dffc6b41866250d2cc3b9969
Nick Kury <nickkury12@gmail.com>
2021-04-07 10:52:21 -0700

    step 3 done

--- mole.js
+++ mole.js
@@ -1,10 +1,33 @@
-// window.addEventListener('DOMContentLoaded', () => {
+ window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        popUpRandomMole();
+    }, 0)
+    const moleHeads = document.querySelectorAll('.wgs');
+    moleHeads.forEach((moleHead) => {
+        moleHead.addEventListener('click', event => {
+            hideMole(moleHead);
+        })
+    })
+ })
 
-//    setInterval(() => {
-//      const moleHeads = document.querySelectorAll('.wgs__mole-head');
-//      for (let moleHead of moleHeads) {
-//        moleHead.classList.toggle('wgs__mole-head--hidden');
-//      }
-//    }, 1000);
 
-//  });
+function popUpRandomMole() {
+    const moleHeads = document.querySelectorAll('.wgs');
+    const random = Math.floor(Math.random() * Math.floor(8));
+    const randomMole = moleHeads[random];
+    randomMole.children[0].classList.remove('wgs__mole-head--hidden');
+    setTimeout(() =>{
+        hideMole(randomMole)
+    }, 3000)
+
+}
+
+function hideMole(mole) {
+    console.log(mole)
+    setTimeout(() => {
+        mole.children[0].classList.add('wgs__mole-head--hidden')
+        
+    }, 1000)
+    popUpRandomMole()
+}
+
