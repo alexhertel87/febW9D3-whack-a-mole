5cf6cf00a36882dd3886606dd918296245bb6bb4
Alex <alex.hertel87@gmail.com>
2021-04-07 14:29:29 -0500

    Basically done with project - bonus time

--- mole.js
+++ mole.js
@@ -2,32 +2,32 @@
     setTimeout(() => {
         popUpRandomMole();
     }, 0)
-    const moleHeads = document.querySelectorAll('.wgs');
+    const moleHeads = document.querySelectorAll('.wgs__mole-head');
     moleHeads.forEach((moleHead) => {
         moleHead.addEventListener('click', event => {
-            hideMole(moleHead);
+           hideMole(moleHead);
+           moleHead.classList.add("wgs__mole-head--whacked")
         })
     })
  })
 
-
 function popUpRandomMole() {
-    const moleHeads = document.querySelectorAll('.wgs');
-    const random = Math.floor(Math.random() * Math.floor(8));
+   const moleHeads = document.querySelectorAll('.wgs__mole-head:not(.wgs__mole-head--whacked)');
+   if (!moleHeads.length) {
+      return
+   }
+    const random = Math.floor(Math.random() * moleHeads.length);
     const randomMole = moleHeads[random];
-    randomMole.children[0].classList.remove('wgs__mole-head--hidden');
+    randomMole.classList.remove('wgs__mole-head--hidden');
     setTimeout(() =>{
         hideMole(randomMole)
-    }, 3000)
-
-}
+    }, 3000)}
 
 function hideMole(mole) {
     console.log(mole)
     setTimeout(() => {
-        mole.children[0].classList.add('wgs__mole-head--hidden')
-        
+        mole.classList.add('wgs__mole-head--hidden')
+
     }, 1000)
     popUpRandomMole()
 }
-
